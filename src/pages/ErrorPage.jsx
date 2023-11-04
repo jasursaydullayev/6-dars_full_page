349ab4b36a9d536d8bead0deddf120b6d2e05cd8
Jasurbek1810 <JasurbekSaydullayev@gmail.com>
2023-11-04 14:21:47 +0500

    half page unsplash

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,21 +1,21 @@
-import { useRouteError, Link } from 'react-router-dom'
+import { useRouteError, Link } from "react-router-dom";
 
 function ErrorPage() {
-    const error = useRouteError()
-    if(error.status === 404){
-        return(
-            <div className="error-page">
-                <h1>Page not Found</h1>
-                <Link to="/">Home</Link>
-            </div>
-        )
-    }
-  return (
-    <div className='error-page'>
-        <h1>Something went wrong :(</h1>
+  const error = useRouteError()
+  if (error.status === 404) {
+    return (
+      <div className="error-page">
+        <h1>Page not Found</h1>
         <Link to="/">Home</Link>
+      </div>
+    );
+  }
+  return (
+    <div className="error-page">
+      <h1>Something went wrong :(</h1>
+      <Link to="/">Home</Link>
     </div>
-  )
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;
